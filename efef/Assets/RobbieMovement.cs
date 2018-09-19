a7ead85c410fe68a67b0f9cc2590da3a30dd95d2
yc2296 <yc2296@cornell.edu>
2018-09-18 20:24:44 -0400

    sample level

---
+++ efef/Assets/RobbieMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobbieMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public float speed;
+    public float horizontalMove;
+    public bool jump = false;
+
+	// Update is called once per frame
+	void Update () {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+	}
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+}
