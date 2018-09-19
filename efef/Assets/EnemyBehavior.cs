bfda2f38fedae3e74d277c677f29516fc59f6d1c
yc2296 <yc2296@cornell.edu>
2018-09-18 22:03:46 -0400

    added AI

--- efef/Assets/EnemyBehavior.cs
+++ efef/Assets/EnemyBehavior.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour {
+    public float walkSpeed = 2.0f;
+    public float wallLeft = -4.0f;
+    public float wallRight = 5.0f;
+    float walkingDirection = 1.0f;
+    Vector2 walkAmount;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+        if (walkingDirection >= 0.0f && transform.position.x >= wallRight) walkingDirection = -1.0f;
+        else if (walkingDirection <= 0.0f && transform.position.x <= wallLeft) walkingDirection = 1.0f;
+        transform.Translate(walkAmount);
+    }
+
 
     private void OnCollisionEnter2D()
     {
