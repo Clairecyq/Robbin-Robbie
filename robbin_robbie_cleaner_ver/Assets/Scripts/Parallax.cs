e30e213ff780158030f9fbf3e0795673e0828c62
Willy Miles <wbm53@cornell.edu>
2018-11-07 16:16:40 -0500

    Added Parallax scrolling

---
+++ robbin_robbie_cleaner_ver/Assets/Scripts/Parallax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    public GameObject background;
+    public float scrollspeed = 1;
+    private Vector3 offset;
+    private Vector3 start;
+
+	// Use this for initialization
+	void Start () {
+        if (background != null)
+        {
+            offset = -transform.position + background.transform.position;
+            start = background.transform.position;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (background != null)
+        {
+            Vector3 newOffset = -transform.position + background.transform.position;
+            Vector3 newPos = ((offset - newOffset) / scrollspeed) + start;
+            background.transform.position = newPos;
+        }
+	}
+}
