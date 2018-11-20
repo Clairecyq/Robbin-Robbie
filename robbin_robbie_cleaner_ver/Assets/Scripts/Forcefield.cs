6d836d255124658aff2380e5556aedf0d81a2697
Eric <mei.eric1@gmail.com>
2018-11-19 21:06:04 -0500

    Switch and forcefield code-- still no corresponding assets

---
+++ robbin_robbie_cleaner_ver/Assets/Scripts/Forcefield.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forcefield : MonoBehaviour {
+
+    public bool isActivated;
+
+	// Use this for initialization
+	void Start () {
+        isActivated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isActivated)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
---
+++ robbin_robbie_cleaner_ver/Assets/Scripts/GameSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSwitch : MonoBehaviour {
+
+    bool isActivated;
+
+    [SerializeField]
+    public GameObject forceField;
+
+    void Start () {
+        isActivated = false;
+	}
+
+    void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.name == "Robbie" && !isActivated)
+        {
+            // gameObject.SetActive(false);
+            //gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            isActivated = true;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            forceField.GetComponent<Forcefield>().isActivated = true;
+        }
+    }
+}
